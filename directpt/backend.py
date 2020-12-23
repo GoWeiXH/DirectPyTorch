实现 recall/precision/f_score 等 metrics 方法；实现自动识别二分类多分类并选取对应 metrics；修复日志打印 bug

directpt/backend.py
@@ -16,17 +16,7 @@ def worst(monitor):
     return best
 
 
-def confusion_matrix(y_pre: torch.Tensor, y_true: torch.Tensor, threshold=0.5):
-    y_pre = torch.as_tensor(y_pre > threshold, dtype=torch.int)
-
-    tp = torch.sum(y_pre * y_true)
-    fn = torch.sum((1 - y_pre) * y_true)
-    fp = torch.sum(y_pre * (1 - y_true))
-    tn = torch.sum((1 - y_pre) * (1 - y_true))
-
-    return tp, fn, fp, tn
+def fill_nan(x, value):
+    return torch.where(torch.isnan(x), torch.full_like(x, value), x)
 
 
-def mc_confusion_matrix(y_pre: torch.Tensor, y_true: torch.Tensor, threshold=0.5, multi_class=False):
-    # todo
-    ...

directpt/data/data.py
@@ -15,7 +15,7 @@ def transform_to_tensor(x):
 
 
 def train_test_split(x_data, y_label, test_size, batch_size,
-                     seed, shuffle, num_workers,
+                     seed=None, shuffle: bool = False, num_workers=1,
                      source_data=None):
     # 数据格式转换
     x_data = transform_to_tensor(x_data)
@@ -26,9 +26,10 @@ def train_test_split(x_data, y_label, test_size, batch_size,
     assert x_len == y_len, "X_data len and Y_label len is unequal (x: {}, y: {})".format(x_len, y_len)
 
     data_idx = list(range(x_len))
-    if shuffle or seed is not None:
-        # 生成样本乱序索引
+    if seed is not None:
         np.random.seed(seed)
+        shuffle = True
+    if shuffle:
         np.random.shuffle(data_idx)
 
     # 划分数据
@@ -54,7 +55,8 @@ def train_test_split(x_data, y_label, test_size, batch_size,
 
 def to_data_loader(x=None, y=None, data_set=None, batch_size=1024, shuffle=False, num_workers=1):
     if x is not None and y is not None:
-        if not data_set:
-            data_set = data.TensorDataset(x, y)
+        data_set = data.TensorDataset(x, y)
+    if not data_set:
+        raise TypeError('x, y or data_set should not be None.')
     return data.DataLoader(dataset=data_set, batch_size=batch_size,
                            shuffle=shuffle, num_workers=num_workers)

directpt/direct/direct.py
@@ -4,10 +4,10 @@ import torch
 import torch.nn as nn
 from torch.utils import data
 
-from ..fit.train import Trainer
+from ..fit import Trainer
 from ..utils import model_summary
-from ..data import train_test_split
 from .._exception import NoCompileError
+from ..data import train_test_split, transform_to_tensor
 
 
 class Direct:
@@ -20,6 +20,7 @@ class Direct:
         self.trainer = None
         self.loss_func = None
         self.optimizer = None
+        self.multi = False
 
     def compile(self, network, loss_func, optimizer, threshold: float = 0.5):
         self.network = network
@@ -54,6 +55,13 @@ class Direct:
         device = 'cpu' if main_device == 'cpu' else 'gpu'
         return main_device, device, is_parallel
 
+    def set_multi_flag(self, y_label):
+        y_label = transform_to_tensor(y_label)
+        if len(y_label.shape) == 1:
+            y_label = y_label.reshape(-1, 1)
+        if y_label.shape[1] > 1:
+            self.multi = True
+
     def fit(self, x_data=None, y_label=None,
             metrics: list = None,
             epochs: int = 1, batch_size=1, val_freq=1,
@@ -66,6 +74,8 @@ class Direct:
         if not self.trainer:
             raise NoCompileError()
 
+        self.set_multi_flag(y_label)
+
         if x_data is not None and y_label is not None:
             # 如果未指定测试数据，则以 test_size 划分测试数据
             if test_data is None:
@@ -73,7 +83,8 @@ class Direct:
                     x_data, y_label,
                     test_size, batch_size,
                     random_seed, shuffle,
-                    num_workers)
+                    num_workers
+                )
 
             # 如果指定测试数据，则无需划分
             else:
@@ -90,5 +101,5 @@ class Direct:
             model_summary(self.network, (x_data.shape[1],), self.device)
 
         self.trainer.train(train_loader, test_loader,
-                           metrics, epochs,
+                           metrics, epochs, self.multi,
                            val_freq, callbacks)

directpt/fit/train.py
@@ -7,7 +7,7 @@ from torch.optim import Optimizer
 from torch.utils.data.dataloader import DataLoader
 
 from ..utils import LogPrinter
-from ..metrics import Correct
+from ..metrics import binary_correct, multi_class_correct
 from .._exception import MetricsError
 
 
@@ -29,8 +29,9 @@ class Trainer:
         self.main_device = main_device
         self.loss_func = loss
         self.optimizer = optimizer
-        self.correct = Correct(threshold=threshold)
+        self.threshold = threshold
 
+        self.correct = None
         self.metric_func_lib = {
             'loss': self.train_loss_step,
             'val_loss': self.test_loss_step,
@@ -55,20 +56,23 @@ class Trainer:
 
     def train_acc_step(self, batch_x: torch.Tensor, batch_y: torch.Tensor) -> float:
         batch_x, batch_y = batch_x.to(self.main_device), batch_y.to(self.main_device)
-        step_correct = self.correct(self.model(batch_x), batch_y)
+        step_correct = self.correct(self.model(batch_x), batch_y, threshold=self.threshold)
         step_acc = step_correct / len(batch_y)
         return step_acc
 
     def test_acc_step(self, batch_x: torch.Tensor, batch_y: torch.Tensor) -> float:
-        step_acc = self.correct(self.model(batch_x), batch_y)
+        step_acc = self.correct(self.model(batch_x), batch_y, threshold=self.threshold)
         return step_acc
 
     def train(self, train_loader: DataLoader, test_loader: DataLoader,
-              metrics: list = None, epochs: int = 1,
+              metrics: list = None, epochs: int = 1, multi: bool = False,
               val_freq=1, callbacks: list = None):
 
         # 初始化日志打印类
         printer = LogPrinter(epochs, len(train_loader), val_freq)
+
+        # 识别 多分类 / 二分类，对应不同计算准确率方法
+        self.correct = multi_class_correct if multi else binary_correct
 
         # 验证评价方法是否合法
         metrics = [] if not metrics else metrics
@@ -146,10 +150,11 @@ class Trainer:
                         steps_loss_list.append(self.test_loss_step(batch_x, batch_y))
 
                 val_acc = correct_num / test_data_len
-                val_loss = torch.mean(torch.tensor(steps_loss_list))
+                if steps_loss_list:
+                    val_loss = torch.mean(torch.tensor(steps_loss_list))
+                    logs['val_loss'].append(val_loss)
 
                 logs['val_acc'].append(val_acc)
-                logs['val_loss'].append(val_loss)
                 printer.add_val_log(logs)
 
             if callbacks:

directpt/metrics/metrics.py
@@ -1,85 +1,64 @@
 import torch
 import torch.nn as nn
 
-from ..backend import confusion_matrix, mc_confusion_matrix
+from ..backend import fill_nan
 
 
-class Correct(nn.Module):
-
-    def __init__(self, threshold=0.5):
-        super(Correct, self).__init__()
-
-        self.threshold = threshold
-
-    def forward(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        y_pre = torch.as_tensor(y_pre > self.threshold, dtype=torch.int)
-        return torch.as_tensor((y_pre == y_true), dtype=torch.int).sum().item()
-
-
-class Accuracy(nn.Module):
-    """
-    计算 二/多分类 准确率
-    """
-
-    def __init__(self, threshold=0.5, multi_class=False):
-        super(Accuracy, self).__init__()
-
-        self.threshold = threshold
-
-        self.correct = Correct()
-
-        if multi_class:
-            self.accuracy_method = self.multi_acc
-        else:
-            self.accuracy_method = self.binary_acc
-
-    def forward(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        return self.accuracy_method(y_pre, y_true)
-
-    def binary_acc(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        correct = self.correct(y_pre, y_true)
-        return correct / len(y_pre)
-
-    def multi_acc(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        y_pre, y_true = torch.argmax(y_pre, dim=1), torch.argmax(y_true, dim=1)
-        correct = self.correct(y_pre, y_true)
-        return correct / len(y_pre)
+def confusion_matrix(y_pre: torch.Tensor, y_true: torch.Tensor,
+                     threshold, multi):
+    y_pre = torch.as_tensor(y_pre > threshold, dtype=torch.int)
+    dim = 1 if multi else 0
+    tp = torch.sum(y_pre * y_true, dim=dim)
+    fn = torch.sum((1 - y_pre) * y_true, dim=dim)
+    fp = torch.sum(y_pre * (1 - y_true), dim=dim)
+    tn = torch.sum((1 - y_pre) * (1 - y_true), dim=dim)
+    return tp, fn, fp, tn
 
 
-class Recall(nn.Module):
-
-    def __init__(self, threshold=0.5, multi_class=False):
-        super(Recall, self).__init__()
-
-        self.threshold = threshold
-
-        if multi_class:
-            self.recall_method = self.multi_recall
-        else:
-            self.recall_method = self.binary_recall
-
-    def forward(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        return self.recall_method(y_pre, y_true)
-
-    def binary_recall(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        y_pre = torch.as_tensor(y_pre > self.threshold, dtype=torch.int)
-        tp, fn, fp, tn = confusion_matrix(y_pre, y_true)
-        return tp / (tp + fn)
-
-    def multi_recall(self, y_pre: torch.Tensor, y_true: torch.Tensor):
-        ...
+def binary_correct(y_pre: torch.Tensor, y_true: torch.Tensor, threshold=0.5):
+    y_pre = torch.as_tensor(y_pre > threshold, dtype=torch.int)
+    same = torch.as_tensor(y_pre == y_true, dtype=torch.int)
+    return torch.sum(same)
 
 
-class Precision(nn.Module):
-    ...
+def multi_class_correct(y_pre: torch.Tensor, y_true: torch.Tensor, threshold=0.5):
+    y_pre, y_true = y_pre.argmax(dim=1), y_true.argmax(dim=1)
+    same = torch.as_tensor(y_pre == y_true, dtype=torch.int)
+    return torch.sum(same)
 
 
-class FBetaScore(nn.Module):
-    ...
+def binary_accuracy(y_pre: torch.Tensor, y_true: torch.Tensor, threshold=0.5):
+    tp, fn, fp, tn = confusion_matrix(y_pre, y_true, threshold, multi=False)
+    return (tp + tn) / (tp + fn + fp + tn)
 
 
-class F1Score(nn.Module):
-    ...
+def multi_class_accuracy(y_pre: torch.Tensor, y_true: torch.Tensor):
+    y_pre, y_true = torch.argmax(y_pre, dim=1), torch.argmax(y_true, dim=1)
+    res = multi_class_correct(y_pre, y_true)
+    return res / len(y_pre)
+
+
+def recall_precision_fscore(y_pre: torch.Tensor, y_true: torch.Tensor,
+                            multi=False,
+                            threshold=0.5, beta=1.0, zero_division=0):
+    if multi:
+        y_pre, y_true = y_pre.T, y_true.T
+
+    tp, fn, fp, _ = confusion_matrix(y_pre, y_true, threshold, multi)
+
+    recall = fill_nan(tp / (tp + fn), zero_division)
+    precision = fill_nan(tp / (tp + fp), zero_division)
+
+    beta2 = beta ** 2
+    f_score = (1 + beta2) * precision * recall / (beta2 * precision + recall)
+    f_score = fill_nan(f_score, zero_division)
+
+    if multi:
+        recall = torch.mean(recall)
+        precision = torch.mean(precision)
+        f_score = torch.mean(f_score)
+
+    return recall, precision, f_score
 
 
 class MacroCostLoss(nn.Module):

dpt_test/main.py
@@ -11,6 +11,7 @@ sys.path.append(cur_path)
 
 from directpt.direct import Direct
 from directpt.callbacks.callbacks import BestSaving
+from directpt.metrics import recall_precision_fscore, binary_accuracy, multi_class_accuracy
 
 
 # y_pre = torch.tensor([1, 1, 1, 0, 0, 0])
@@ -18,13 +19,13 @@ from directpt.callbacks.callbacks import BestSaving
 
 # y_pre = torch.tensor([[1, 0, 0, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]])
 # y_true = torch.tensor([[1, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-#
-# method = me.Recall(multi_class=True)
-# res = method(y_pre, y_true)
-# print()
 
-# accuracy = me.MCAccuracy()
-# acc = accuracy(y_pre, y_true)
+# y_pre = torch.tensor([[1, 1, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 1]])
+# y_true = torch.tensor([[0, 0, 1, 0], [1, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]])
+
+
+# r, p, f = recall_precision_fscore(y_pre, y_true, multi=False)
+# acc = binary_accuracy(y_pre, y_true)
 # print()
 
 
@@ -32,10 +33,9 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
         self.gen = nn.Sequential(
-            nn.BatchNorm1d(256),
             nn.Linear(256, 128),
-            nn.Linear(128, 1),
-            nn.ReLU()
+            nn.Linear(128, 2),
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -49,15 +49,18 @@ if __name__ == '__main__':
     x_n = torch.zeros(5000, 256).float()
     x_data = torch.cat((x_p, x_n))
 
-    y_p = torch.ones(5000, 1).float()
-    y_n = torch.zeros(5000, 1).float()
-    y_label = torch.cat((y_p, y_n))
+    y_1 = torch.tensor([0, 1]).repeat(5000, 1).float()
+    y_2 = torch.tensor([1, 0]).repeat(5000, 1).float()
+    y_label = torch.cat((y_1, y_2))
+    # y_p = torch.ones(5000, 1).float()
+    # y_n = torch.zeros(5000, 1).float()
+    # y_label = torch.cat((y_p, y_n))
 
     # ------ 模型 ------
     # 创建 模型
     gen = Generator()
     # 创建 优化器
-    opt = torch.optim.SGD(params=gen.parameters(), lr=1e-2, momentum=0.9)
+    opt = torch.optim.SGD(params=gen.parameters(), lr=1e-5, momentum=0.9)
     # 创建 损失函数
     loss = torch.nn.MSELoss()
     # 回调函数
@@ -66,5 +69,5 @@ if __name__ == '__main__':
     direct = Direct()
     direct.compile(gen, loss, opt, threshold=0.5)
     direct.fit(x_data, y_label, metrics=['acc', 'val_acc', 'val_loss'],
-               epochs=20, batch_size=20,
+               epochs=20, batch_size=50,
                callbacks=[best_saving])